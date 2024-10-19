7375cbc188d5e30b0cee0ffebd69968597e956c6
luiscc02 <luis.calderon@mail.utoronto.ca>
2024-10-19 14:45:47 -0400

    Task 2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -9,6 +9,7 @@ public interface LoginUserDataAccessInterface {
 
     /**
      * Checks if the given username exists.
+     *
      * @param username the username to look for
      * @return true if a user with the given username exists; false otherwise
      */
@@ -16,16 +17,30 @@ public interface LoginUserDataAccessInterface {
 
     /**
      * Saves the user.
+     *
      * @param user the user to save
      */
     void save(User user);
 
     /**
      * Returns the user with the given username.
+     *
      * @param username the username to look up
      * @return the user with the given username
      */
     User get(String username);
 
+    /**
+     * Sets the current user.
+     *
+     * @param name the name of the current user
+     */
     void setCurrentUser(String name);
+
+    /**
+     * Gets the current user's name.
+     *
+     * @return the name of the current user
+     */
+    String getCurrentUser();
 }
